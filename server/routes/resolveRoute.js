dea9c3a9fc66c3a83ea95c7f51572ddb35647a40
kunark <kunark.khewal@tothenew.com>
2019-06-30 19:40:40 +0530

    nodemailer is applied on complaint resolve

--- server/routes/resolveRoute.js
+++ server/routes/resolveRoute.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const resolveOperation = require('../database/controller/resolveOperations');
+const mailer = require('../config/nodemailer');
 
 router.get('/', (req, res) => {
     resolveOperation.fetchComplaint(req.user.emailId)
@@ -14,6 +15,19 @@ router.get('/', (req, res) => {
 router.patch('/', (req, res) => {
     resolveOperation.updateComplaint(req.body.status, req.body.issueId)
         .then(data => {
+            if(data.status === "Resolved"){
+                mailer({
+                    issueId: data.issueId,
+                    attachment: data.attachment,
+                    department: data.department,
+                    emailId: data.emailId,
+                    name: data.name,
+                    concern: data.concern,
+                    title: data.title,
+                    subject: "Your Complaint is Resolved",
+                    text: "Thank You for your support!"
+                });
+            }
             res.status(200).send(data);
         })
         .catch(err => {
